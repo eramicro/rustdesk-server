e8bf9fbc189ed7a38372295d846464a61e62d77d
eramicro <645452175@qq.com>
2024-09-06 16:59:32 +0800

    utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -109,12 +109,12 @@ fn doctor_ip(server_ip_address: std::net::IpAddr, server_address: Option<&str>) 
     // TODO: ICMP ping?
 
     // port check TCP (UDP is hard to check)
-    doctor_tcp(server_ip_address, "21114", "API");
-    doctor_tcp(server_ip_address, "21115", "hbbs extra port for nat test");
-    doctor_tcp(server_ip_address, "21116", "hbbs");
-    doctor_tcp(server_ip_address, "21117", "hbbr tcp");
-    doctor_tcp(server_ip_address, "21118", "hbbs websocket");
-    doctor_tcp(server_ip_address, "21119", "hbbr websocket");
+    doctor_tcp(server_ip_address, "11114", "API");
+    doctor_tcp(server_ip_address, "11115", "hbbs extra port for nat test");
+    doctor_tcp(server_ip_address, "11116", "hbbs");
+    doctor_tcp(server_ip_address, "11117", "hbbr tcp");
+    doctor_tcp(server_ip_address, "11118", "hbbs websocket");
+    doctor_tcp(server_ip_address, "11119", "hbbr websocket");
 
     // TODO: key check
 }
